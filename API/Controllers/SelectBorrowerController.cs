73bab68aed32dcf5c0c4c379e5a1c124eab5f5f7
KHIA LECH <102273448@student.swin.edu.au>
2019-11-27 13:20:01 +1100

    Step5

---
+++ API/Controllers/SelectBorrowerController.cs
@@ -0,0 +1,67 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class SelectedBorrower : ApiController
+    {
+        // GET: Api/SelectedBorrower
+
+        public IEnumerable<Borrower> Get()
+        {
+            SqlConnection conn = DBConnection.GetConnection();
+            SqlCommand cmd;
+            SqlDataReader rdr;
+            string query;
+            List<SelectedBorrower> output = new List<SelectedBorrower>();
+
+
+            try
+            {
+                conn.Open();
+
+                query = "select * from Borrower";
+
+                cmd = new SqlCommand(query, conn);
+
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+
+                    output.Add(new SelectedBorrower(Int32.Parse(rdr.GetValue(0).ToString()),
+                        rdr.GetValue(1).ToString(),
+                        rdr.GetValue(2).ToString(),
+                        rdr.GetValue(3).ToString(),
+                            new Books(Int32.Parse(rdr.GetValue(0).ToString()),
+                            rdr.GetValue(1).ToString(),
+                            Int32.Parse(rdr.GetValue(2).ToString(),
+                            Int32.Parse(rdr.GetValue(3).ToString())))));
+
+
+                }
+
+            }
+
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                    conn.Close();
+            }
+            return output;
+
+        }
+
+
+    }
+}
---
+++ API/Models/SelectedBorrower.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class SelectedBorrower
+    {
+        public int id { get; set; }
+        public string surname { get; set; }
+        public string firstname { get; set; }
+        public string DOB { get; set; }
+        public Books books { get; set; }
+
+        public SelectedBorrower(int sbid, string sbsurname, string sbfirstname, string sbDOB, Books bbooks)
+        {
+            id = sbid;
+            surname = sbsurname;
+            firstname = sbfirstname;
+            DOB = sbDOB;
+            books = bbooks;
+
+
+        }
+
+        public SelectedBorrower()
+        {
+
+        }
+    }
+}
